d059c80348e59ad61a744c10f793f1ed1ce56a4f
egurinko <egctoru@gmail.com>
2020-03-31 22:48:27 +0900

    added theme

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -3,14 +3,14 @@ import { createMuiTheme } from '@material-ui/core/styles';
 export default createMuiTheme({
   palette: {
     primary: {
-      light: 'hsla(15, 90%, 60%, 1)',
-      main: 'hsla(15, 70%, 60%, 1)',
-      dark: 'hsla(15, 70%, 40%, 1)',
-    },
-    secondary: {
       light: 'hsla(180, 60%, 65%, 1)',
       main: 'hsla(180, 40%, 65%, 1)',
       dark: 'hsla(180, 40%, 45%, 1)',
+    },
+    secondary: {
+      light: 'hsla(15, 90%, 60%, 1)',
+      main: 'hsla(15, 70%, 60%, 1)',
+      dark: 'hsla(15, 70%, 40%, 1)',
     },
     background: {
       default: 'hsla(170, 50%, 50%, 1)',
