3b7d49f9be3f9442dc4a3c8583953d307e8ce0c5
egurinko <egctoru@gmail.com>
2020-03-31 22:47:38 +0900

    added fetcher for type fetch

---
+++ src/utils/fetcher.ts
@@ -0,0 +1,30 @@
+import fetch from 'isomorphic-unfetch';
+
+const wrap = <T>(task: Promise<Response>): Promise<T> => {
+  return new Promise((resolve, reject) => {
+    task
+      .then((response) => {
+        if (response.ok) {
+          response
+            .json()
+            .then((json) => {
+              resolve(json);
+            })
+            .catch((error) => {
+              reject(error);
+            });
+        } else {
+          reject(response);
+        }
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+};
+
+const fetcher = <T = any>(input: RequestInfo, init?: RequestInit): Promise<T> => {
+  return wrap<T>(fetch(input, init));
+};
+
+export default fetcher;
