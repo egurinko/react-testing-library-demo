306b48504340dc0e769f0dc42b33bc16779d565b
egurinko <egctoru@gmail.com>
2020-03-31 22:51:42 +0900

    used container insteadof componet

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { CssBaseline } from '@material-ui/core';
 import NotFound from './components/NotFound';
-import Index from './components/Index';
+import Index from './containers/index';
 import Loader from './containers/common/loader';
 
 const App: React.FC<{}> = () => (
