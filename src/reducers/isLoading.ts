37fd3e39b45973f04ba5ccac7940a085041df8e0
egurinko <egctoru@gmail.com>
2020-03-31 22:51:27 +0900

    changed loading state when fetching pokemon

--- src/reducers/isLoading.ts
+++ src/reducers/isLoading.ts
@@ -3,6 +3,11 @@ import { ActionTypes } from '../actions/actionTypes';
 
 const isLoading = (state = false, action: Actions): boolean => {
   switch (action.type) {
+    case ActionTypes.FETCH_POKEMONS_START:
+      return true;
+    case ActionTypes.FETCH_POKEMONS_SUCCESS:
+    case ActionTypes.FETCH_POKEMONS_FAILURE:
+      return false;
     default:
       return state;
   }
