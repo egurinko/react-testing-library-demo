7b74b27d93d4c6d827779b230aad6c66aaf7dc09
egurinko <egctoru@gmail.com>
2020-04-04 16:31:12 +0900

    remove source-map warning

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  devtool: 'inline-source-map',
   entry: './src/index',
   output: {
     path: path.join(__dirname, '/dist'),
