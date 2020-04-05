36bb5a3bedac154acc1b717f4f474f744cd89dc4
egurinko <egctoru@gmail.com>
2020-04-05 13:43:39 +0900

    make empty

---
+++ src/components/pokemons/detail/Empty.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Typography, Grid, Chip } from '@material-ui/core';
+import { withStyles, WithStyles, Theme, StyleRules } from '@material-ui/core/styles';
+import { Pokemon } from '../../../actions/thunks/types/fetchPokemons';
+
+const styles = (theme: Theme): StyleRules => ({
+  heroImage: {
+    width: 300,
+    margin: `0 ${theme.spacing(3)}px`,
+  },
+  heroCard: {
+    width: 400,
+    height: 300,
+    margin: `0 ${theme.spacing(3)}px`,
+    verticalAlign: 'middle',
+  },
+  heroId: {
+    marginBottom: theme.spacing(2),
+  },
+  heroName: {
+    marginBottom: theme.spacing(3),
+  },
+  chip: {
+    marginRight: theme.spacing(2),
+    height: 40,
+    fontSize: theme.typography.h6.fontSize,
+  },
+});
+
+type OwnProps = {
+  pokemon: Pokemon;
+};
+
+type EmptyProps = OwnProps & WithStyles<typeof styles>;
+
+const Empty: React.FC<EmptyProps> = ({ classes, pokemon }) => (
+  <>
+    <img src={pokemon.sprites.front_default!} className={classes.heroImage} />
+    <Grid container direction="column" justify="center" className={classes.heroCard}>
+      <Typography variant="h4" className={classes.heroId}>
+        No. {pokemon.id}
+      </Typography>
+      <Typography variant="h2" className={classes.heroName}>
+        {pokemon.name}
+      </Typography>
+      <div>
+        {pokemon.types.map((type) => (
+          <Chip size="medium" label={type.type.name} key={type.slot} className={classes.chip} />
+        ))}
+      </div>
+    </Grid>
+  </>
+);
+
+export default withStyles(styles)(Empty);
