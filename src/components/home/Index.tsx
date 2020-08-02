582e0c12aec8faa3c1f7dfb0afca08ad78280099
egurinko <egctoru@gmail.com>
2020-08-03 02:26:46 +0900

    change home to test basic click

---
+++ src/components/home/Index.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import '../../stylesheets/home/index.css';
+
+const descriptions = [
+  {
+    title: 'My Pokemon',
+    shortened: 'There are 3 functions.',
+    full:
+      'You can check pokemons in Pokedex page. If you like one of them, go the detail page and add to My Pokemons. My Pokemons could be seen in My Pokemons page.',
+  },
+  {
+    title: 'The name of Pokémon',
+    shortened:
+      'is the portmanteau of the Japanese brand Pocket Monsters. The term "Pokémon", in addition to referring to the Pokémon franchise itself,',
+    full:
+      'also collectively refers to the 896 fictional species that have made appearances in Pokémon media as of the release of the eighth generation titles Pokémon Sword and Shield. "Pokémon" is identical in the singular and plural, as is each individual species name; it is grammatically correct to say "one Pokémon" and "many Pokémon", as well as "one Pikachu" and "many Pikachu".',
+  },
+];
+
+const Index: React.FC<{}> = () => {
+  const [showFullDescriptions, setShowFullDescriptions] = useState(
+    [...descriptions].map(() => false),
+  );
+
+  const handleReadMore = (index: number): void => {
+    const nextShowFullDescriptions = showFullDescriptions.map((prevValue, prevIndex) =>
+      prevIndex === index ? true : prevValue,
+    );
+    setShowFullDescriptions(nextShowFullDescriptions);
+  };
+
+  return (
+    <div>
+      <h1 className="home">Welcome to Pokemon world!</h1>
+      {descriptions.map((description, index) => (
+        <div className="home_description" key={description.title}>
+          <h2 className="home__description__title">{description.title}</h2>
+          <h4>
+            {description.shortened}
+            <button
+              type="button"
+              onClick={() => handleReadMore(index)}
+              className={showFullDescriptions[index] ? 'none' : 'button'}
+            >
+              Read more...
+            </button>
+            <span className={showFullDescriptions[index] ? '' : 'home__description__full'}>
+              {description.full}
+            </span>
+          </h4>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Index;
