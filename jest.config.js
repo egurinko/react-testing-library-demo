acc82434db1d759966085d9b6bfced2e6ae765e8
egurinko <egctoru@gmail.com>
2020-08-22 17:05:08 +0900

    add static assets mock

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/src/__test__/config/importJestDOM.ts']
+  setupFilesAfterEnv: ['<rootDir>/src/__test__/config/importJestDOM.ts'],
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/__test__/mock/fileMock.ts",
+    "\\.(css|less)$": "<rootDir>/src/__test__/mock/styleMock.ts"
+  }
 };
