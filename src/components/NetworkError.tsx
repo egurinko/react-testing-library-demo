7ca574a757bddd2aa2fd0e7944936962e9b80e7c
egurinko <egctoru@gmail.com>
2020-08-22 13:29:45 +0900

    create NetworkError.tsx for Material UI test

--- src/components/NetworkError.tsx
+++ src/components/NetworkError.tsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import { withStyles, WithStyles, Theme, StyleRules } from '@material-ui/core/styles';
+import { Typography, Grid } from '@material-ui/core';
 
-const NetworkError: React.FC<{}> = () => (
-  <div>
-    <h1>Network Error</h1>
-  </div>
+const styles = (theme: Theme): StyleRules => ({
+  title: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo: {
+    width: 400,
+  },
+  detail: {
+    textAlign: 'center',
+  },
+});
+
+type NetworkErrorProps = WithStyles<typeof styles>;
+
+const NetworkError: React.FC<NetworkErrorProps> = ({ classes }) => (
+  <Grid container justify="space-around">
+    <Grid item xs={6} className={classes.title}>
+      <Typography variant="h1">Pica!!</Typography>
+    </Grid>
+    <Grid item xs={6}>
+      <img
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
+        alt="picachu"
+        className={classes.logo}
+      />
+    </Grid>
+    <Grid item xs={12} className={classes.detail}>
+      <Typography variant="h5">Maybe internet connection is not good.</Typography>
+    </Grid>
+  </Grid>
 );
 
-export default NetworkError;
+export default withStyles(styles)(NetworkError);
